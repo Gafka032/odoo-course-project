7ed4ea0b980de9927e731b70c7a8ba59d6c66c91
danylo <danulo.senuk@gmail.com>
2025-05-11 15:04:55 +0300

    [ADD] courier_delivery: add new module

---
+++ courier_delivery/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name' : 'Courier Delivery',
+    'version' : '17.0.0.0.1',
+    'description' : """
+        Courier Delivery
+        This module allows you create request for courier pickup and create
+        delivery orders
+    """,
+    'category' : 'Services',
+    'author' : "Odoo School",
+    'website': 'https://odoo.school/',
+    'license': 'OPL-1',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'auto_install': False,
+    'application': True,
+    'installable': True,
+    'maintainer' : 'Danylo Senyuk',
+    'images' : [],
+
+}
